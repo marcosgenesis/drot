67a705a1b9d076dcdf19f8a077cc38b277cd386b
ericrodriguesfer <er601432@gmail.com>
2021-03-03 09:37:45 -0300

    Configuration connection database MySQL

--- src/main/java/br/com/pi/drot/App.java
+++ src/main/java/br/com/pi/drot/App.java
@@ -1,7 +1,25 @@
-package br.com.pi.drot;;
+package br.com.pi.drot;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.pi.drot.model.Cliente;
 
 public class App {
-	public static void main(String[] args) {
 
+	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BancoJPA");
+	private static EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Teste");
+
+		//entityManager.getTransaction().begin();
+		//entityManager.persist(cliente);
+		//entityManager.getTransaction().commit();
+
+		cliente = entityManager.find(Cliente.class, 5);
+		System.out.println("Nome do cliente: " + cliente.getNome());
 	}
 }
---
+++ src/main/java/br/com/pi/drot/model/Cliente.java
@@ -0,0 +1,38 @@
+package br.com.pi.drot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Clientes")
+
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "nome")
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
