2dd23a1c4e178d9d2c69d4eee5d6f9f35ce8382d
Ítalo Lima <italolima534@gmail.com>
2021-03-13 11:03:02 -0300

    Creation of screen and controller for new user

---
+++ src/main/java/br/com/pi/drot/controllers/NovoUsuarioFXController.java
@@ -0,0 +1,28 @@
+package br.com.pi.drot.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class NovoUsuarioFXController implements Initializable{
+
+	@FXML
+	private Button clickBtn;
+
+	@FXML
+	private void btnAction(ActionEvent event) {
+		JOptionPane.showMessageDialog(null, "O controller da tela de cadastro de novo usuário funcionou!", "Controller implementado com sucesso!", 0);
+	}
+
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+}
+
