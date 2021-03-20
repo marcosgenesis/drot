814d0c1a00019782c2b5ffe63f4a15922ba18066
ericrodriguesfer <er601432@gmail.com>
2021-03-20 15:30:52 -0300

    refactoring class Controller NovoUsuarioFXController

--- src/main/java/br/com/pi/drot/controllers/NovoUsuarioFXController.java
+++ src/main/java/br/com/pi/drot/controllers/NovoUsuarioFXController.java
@@ -2,6 +2,7 @@ package br.com.pi.drot.controllers;
 
 import br.com.pi.drot.dto.UsuarioDTO;
 import br.com.pi.drot.entity.Usuario;
+import br.com.pi.drot.utils.NameDataBaseConnection;
 
 public class NovoUsuarioFXController {
 
@@ -14,8 +15,9 @@ public class NovoUsuarioFXController {
 		usuario.setEndereco("Quixadá-CE");
 		usuario.setTelefone("777");
 
+		NameDataBaseConnection nameDataBaseConnection = new NameDataBaseConnection("myself-mysql");
 
-		UsuarioDTO usuarioDTO = new UsuarioDTO();
+		UsuarioDTO usuarioDTO = new UsuarioDTO(nameDataBaseConnection);
 		usuarioDTO.cadastrarUsuario(usuario);
 
 		return true;
