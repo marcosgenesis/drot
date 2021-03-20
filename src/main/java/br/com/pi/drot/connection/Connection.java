1d4460d7806898a2099758c8df10f9491c9e8a54
ericrodriguesfer <er601432@gmail.com>
2021-03-20 15:07:40 -0300

    fixing connection database

--- src/main/java/br/com/pi/drot/connection/Connection.java
+++ src/main/java/br/com/pi/drot/connection/Connection.java
@@ -4,22 +4,30 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import br.com.pi.drot.model.Cliente;
-import br.com.pi.drot.model.Usuario;
-
 public class Connection {
 
-	private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("BancoJPA");
-	private static EntityManager entityManager = entityManagerFactory.createEntityManager();
+	private EntityManagerFactory entityManagerFactory;
+	private EntityManager entityManager;
 
-	public static void main(String[] args) {
-		Usuario usuario = new Usuario(1, "Usuário Teste", null, null, null, null, null, false);
-
-		//entityManager.getTransaction().begin();
-		//entityManager.persist(cliente);
-		//entityManager.getTransaction().commit();
-
-		usuario = entityManager.find(Usuario.class, 5);
-		System.out.println("Nome do cliente: " + usuario.getNome());
+	public Connection(String dataBase) {
+		this.entityManagerFactory = Persistence.createEntityManagerFactory(dataBase);
+		this.entityManager = entityManagerFactory.createEntityManager();
 	}
+
+	public EntityManagerFactory getEntityManagerFactory() {
+		return entityManagerFactory;
+	}
+
+	public void setEntityManagerFactory(EntityManagerFactory entityManagerFactory) {
+		this.entityManagerFactory = entityManagerFactory;
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
 }
