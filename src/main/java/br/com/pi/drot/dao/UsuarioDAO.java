ca140107bde4d8de3c619023dc91cd3fed7e6871
ericrodriguesfer <er601432@gmail.com>
2021-03-21 00:18:13 -0300

    fixing structure tha classes

--- src/main/java/br/com/pi/drot/dao/UsuarioDAO.java
+++ src/main/java/br/com/pi/drot/dao/UsuarioDAO.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 
 import br.com.pi.drot.entity.Usuario;
 
-
 public interface UsuarioDAO {
+
 	boolean cadastrarUsuario(Usuario usuario);
+
 	Usuario buscarUsuarioPorID(int id);
+
 	ArrayList<Usuario> listarUsuarios();
+
 	boolean editar(Usuario usuario);
+
 	boolean removerPorId(int id);
+
 	boolean removerPorUsuario(Usuario usuario);
+
 }
--- src/main/java/br/com/pi/drot/dto/UsuarioDTO.java
+++ src/main/java/br/com/pi/drot/dto/UsuarioDTO.java
@@ -7,7 +7,7 @@ import br.com.pi.drot.dao.UsuarioDAO;
 import br.com.pi.drot.entity.Usuario;
 import br.com.pi.drot.utils.NameDataBaseConnection;
 
-public class UsuarioDTO implements UsuarioDAO{
+public class UsuarioDTO implements UsuarioDAO {
 
 	private Connection connection;
 
@@ -23,36 +23,41 @@ public class UsuarioDTO implements UsuarioDAO{
 		this.connection = connection;
 	}
 
+	@Override
 	public boolean cadastrarUsuario(Usuario usuario) {
 		this.getConnection().getEntityManager().getTransaction().begin();
 		this.getConnection().getEntityManager().persist(usuario);
 		this.getConnection().getEntityManager().getTransaction().commit();
-
 		this.getConnection().getEntityManager().close();
 
 		return true;
 	}
 
+	@Override
 	public Usuario buscarUsuarioPorID(int id) {
 		return null;
 	}
 
+	@Override
 	public ArrayList<Usuario> listarUsuarios() {
 		this.getConnection().getEntityManager();
-		ArrayList<Usuario> usuarios= (ArrayList<Usuario>) this.getConnection().getEntityManager().createQuery("from Usuario", Usuario.class).getResultList();
+		ArrayList<Usuario> usuarios = (ArrayList<Usuario>) this.getConnection().getEntityManager().createQuery("from Usuario", Usuario.class).getResultList();
 		this.getConnection().getEntityManager().close();
 
 		return usuarios;
 	}
 
+	@Override
 	public boolean editar(Usuario usuario) {
 		return false;
 	}
 
+	@Override
 	public boolean removerPorId(int id) {
 		return false;
 	}
 
+	@Override
 	public boolean removerPorUsuario(Usuario usuario) {
 		return false;
 	}
